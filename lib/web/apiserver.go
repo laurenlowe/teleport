631b4350416a1716502e34305a1221c0b86b465a
joerger <bjoerger@goteleport.com>
2025-09-17 17:40:57 -0700

    Refactor server session ID logic; Separate join and open session flows.

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -4174,6 +4174,7 @@ func (h *Handler) generateSession(ctx context.Context, req *TerminalRequest, clu
 		ServerHostname: host,
 		ServerHostPort: port,
 		Moderated:      accessEvaluator.IsModerated(),
+		ID:             session.NewID(),
 		Created:        time.Now().UTC(),
 		LastActive:     time.Now().UTC(),
 		Namespace:      apidefaults.Namespace,
