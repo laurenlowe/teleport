e378b4f6bbc90c29a7701bbbb66890a683a3ef4b
Dan Upton <daniel.upton@goteleport.com>
2025-11-10 16:11:33 +0000

    Add `DelegationSessionID` lock target

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -3338,6 +3338,7 @@ func generateCert(ctx context.Context, a *Server, req internal.CertRequest, caTy
 		deviceID:             req.DeviceExtensions.DeviceID,
 		botInstanceID:        req.BotInstanceID,
 		joinToken:            req.JoinToken,
+		delegationSessionID:  req.DelegationSessionID,
 	}); err != nil {
 		return nil, trace.Wrap(err)
 	}
@@ -3879,6 +3880,9 @@ type verifyLocksForUserCertsReq struct {
 	botInstanceID string
 	// joinMethod is the join token name, set only for non-token bots.
 	joinToken string
+	// delegationSessionID is the ID of a Delegation Session, when using access
+	// delegation.
+	delegationSessionID string
 }
 
 // verifyLocksForUserCerts verifies if any locks are in place before issuing new
@@ -3908,6 +3912,9 @@ func (a *Server) verifyLocksForUserCerts(req verifyLocksForUserCertsReq) error {
 	}
 	if req.joinToken != "" {
 		lockTargets = append(lockTargets, types.LockTarget{JoinToken: req.joinToken})
+	}
+	if req.delegationSessionID != "" {
+		lockTargets = append(lockTargets, types.LockTarget{DelegationSessionID: req.delegationSessionID})
 	}
 
 	return trace.Wrap(a.checkLockInForce(lockingMode, lockTargets))
